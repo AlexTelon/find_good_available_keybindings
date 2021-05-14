6b68d6c7a3e019988c3c80e9672541215461296b
Alex Telon <alex.telon@gmail.com>
2021-05-14 15:51:33 +0200

    Now presenting nice suggestions!
    After filtering for various things it is now possible to produce a nice
    list of options.
    
    Here are the 10 least used presented according to my keyboard layout
    together with how often they occur in english words.
    
        Ctrl + K 12
        Ctrl + R 11
        Shift + X 10
        Ctrl + D 9
        Ctrl + J 8
        Ctrl + C 5
        Ctrl + S 3
        Ctrl + W 2
        Alt + X 1
        Ctrl + V 1
    
    Alt + X and Ctrl + V only occur once. These correspond to kx and jv.
    While there are 2 Alts and Ctrls there is a filtering rule saying it
    must use both sides of the keyboard. hence Alt + X is not dx for
    instance.
    
    Next step would be to filter out combinations, like Ctrl + V, that are
    already in use.

--- solve.py
+++ solve.py
@@ -51,7 +51,45 @@ if __name__ == '__main__':
     print(len(most_common))
     print(most_common[:2])
     print(most_common[-2:])
+    print()
 
+    # left - right side combo. So if first is on left, then second must be on right
+    is_left = lambda c: c in left_side_letters
+    #is_right = lambda c: c in right_side_letters
+    both_sides = lambda pair: is_left(pair[0]) != is_left(pair[1])
+
+    most_common = [(pair,count) for pair, count in most_common if both_sides(pair)]
+    print(len(most_common))
+    print(most_common[:2])
+    print(most_common[-2:])
+
+    # TODO add special chars as unused pairs in most_common? sure . and ; are used somewhat but still they should be added
+
+    # We still have plenty of options here. Lets narrow down to only use letters on the
+    # easiest to reach rows. Those where SDF and JKL reside.
+    # again removed special chars.
+    easy_to_reach = 'wersdfxcv' + 'uiojklm'
+    is_easy = lambda c: c in easy_to_reach
+    # Note that the first letter is on the homerow already so wont check that.
+    most_common = [(pair,count) for pair, count in most_common if is_easy(pair[1])]
+    print(len(most_common))
+    print(most_common[:2])
+    print(most_common[-2:])
+    print()
+
+    def as_shortcut(pair):
+        translate_table = {
+            's': 'Shift',
+            'l': 'Shift',
+            'd': 'Alt',
+            'k': 'Alt',
+            'f': 'Ctrl',
+            'j': 'Ctrl',
+        }
+        return f"{translate_table[pair[0]]} + {pair[1].upper()}"
+
+    for pair, count in most_common[-10:]:
+        print(as_shortcut(pair))
 
     # both_in_homerow = lambda pair: pair[0] in home_row and pair[1] in home_row
     # End goal.

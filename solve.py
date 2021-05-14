99d5249f42f7e5cbf93cb7d7a16609e2953ec493
Alex Telon <alex.telon@gmail.com>
2021-05-14 15:28:13 +0200

    Filtering among the solutions

--- solve.py
+++ solve.py
@@ -25,7 +25,35 @@ if __name__ == '__main__':
     words = random.sample(words, 2000)
 
     c_pairs = [c for word in words for c in char_pairs(word)]
-    count = Counter(c_pairs)
+    counter = Counter(c_pairs)
 
-    print(count.most_common(5))
-    print(count.most_common()[-5:])
+    # not a true homerow, but these are the dual function keys I have and care about right now.
+    home_row = 'sdfjkl'
+    home_row_left, home_row_right = 'sdf', 'jkl'
+
+    left_side_letters =  'qwertasdfgzxcvb'
+    right_side_letters = 'yuiophjklnm' # removed special chars, they are not present in words anyways
+
+
+    most_common = counter.most_common()
+    print(len(most_common))
+    print()
+
+    # Remove last letter pairs.
+    most_common = [(pair,count) for pair, count in most_common if pair[1] != '']
+    print(len(most_common))
+    print(most_common[:2])
+    print(most_common[-2:])
+    print()
+
+    # Only care if first is in home row.
+    most_common = [(pair,count) for pair, count in most_common if pair[0] in home_row]
+    print(len(most_common))
+    print(most_common[:2])
+    print(most_common[-2:])
+
+
+    # both_in_homerow = lambda pair: pair[0] in home_row and pair[1] in home_row
+    # End goal.
+    # home_row_side + letter on other side that is easy to reach
+    # Then manually choose what feels good and is not already taken.
